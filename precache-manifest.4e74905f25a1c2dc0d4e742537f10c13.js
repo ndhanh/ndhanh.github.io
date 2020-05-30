a4c0813b29909ecca88f33d2a3cecee9708009d4
Hanh Nguyen <hanh.nguyen@guidantfinancial.com>
2020-05-30 14:58:12 +0700

    Updates

---
+++ precache-manifest.4e74905f25a1c2dc0d4e742537f10c13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72c6b681aafdf8e225179508b1a7675",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e3fb89f71805230e2d5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ca6ea9819dbdac165d0",
+    "url": "/static/js/2.f7fbe370.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7fbe370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e3fb89f71805230e2d5",
+    "url": "/static/js/main.fa5f7b69.chunk.js"
+  },
+  {
+    "revision": "0c0c1183850c7b4931f6",
+    "url": "/static/js/runtime-main.92a66312.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
